d1ece5fa4765465e55f2ff4ce820b0ad298a3f11
Code1deep <az.bahya@gmail.com>
2025-06-09 15:59:38 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# app/config.py
-import os
-
-class Config:
-    # Utilisez SQLite par défaut (plus simple pour commencer)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'family.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'votre-cle-secrete-ici'
